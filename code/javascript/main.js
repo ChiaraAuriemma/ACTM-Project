9ffe0bdbe5dadd7e30a9b26b9ac8a175f728e195
ChiaraAuriemma <chiara.auriemma@mail.polimi.it>
2023-11-29 15:54:51 +0100

    aggiornamenti pagina e menù a tendina

--- code/javascript/main.js
+++ code/javascript/main.js
@@ -1,63 +1,26 @@
-document.getElementById("piano_button").addEventListener("click", (e) => {
-    instrument = document.createElement("div");
-    instrument.setAttribute("class","instrument");
-    instruments_div = document.getElementById("instruments_div");
-    instruments_div.appendChild(instrument);
+function choose_instrument(event) {
+  button = event.target;
+  string = button.getAttribute("id");
 
-    image_div= document.createElement("div");
-    image = document.createElement("img");
-    image.setAttribute("src", "images/piano.jpg")
-    instrument.appendChild(image_div);
-    image_div.appendChild(image);
+  instrument = document.createElement("div");
+  instrument.setAttribute("class","instrument");
+  instruments_div = document.getElementById("instruments_div");
+  instruments_div.appendChild(instrument);
 
-})
+  image_div= document.createElement("div");
+  image = document.createElement("img");
+  image.setAttribute("src", "images/" + string + ".jpg")
+  instrument.appendChild(image_div);
+  image_div.appendChild(image);
+}
 
-document.getElementById("drum_button").addEventListener("click", (e) => {
-    instrument = document.createElement("div");
-    instrument.setAttribute("class","instrument");
-    instruments_div = document.getElementById("instruments_div");
-    instruments_div.appendChild(instrument);
-
-    image_div= document.createElement("div");
-    image = document.createElement("img");
-    image.setAttribute("src", "images/drum.jpg")
-    instrument.appendChild(image_div);
-    image_div.appendChild(image);
-
-})
-
-document.getElementById("guitar_button").addEventListener("click", (e) => {
-    instrument = document.createElement("div");
-    instrument.setAttribute("class","instrument");
-    instruments_div = document.getElementById("instruments_div");
-    instruments_div.appendChild(instrument);
-
-    image_div= document.createElement("div");
-    image = document.createElement("img");
-    image.setAttribute("src", "images/guitar.jpg")
-    instrument.appendChild(image_div);
-    image_div.appendChild(image);
-
-})
-
-document.getElementById("bass_button").addEventListener("click", (e) => {
-    instrument = document.createElement("div");
-    instrument.setAttribute("class","instrument");
-    instruments_div = document.getElementById("instruments_div");
-    instruments_div.appendChild(instrument);
-
-    image_div= document.createElement("div");
-    image = document.createElement("img");
-    image.setAttribute("src", "images/bass.jpg")
-    instrument.appendChild(image_div);
-    image_div.appendChild(image);
-
-})
-
-function toggleDropdown() {
-    const dropdownContent = document.getElementById('dropdownContent');
+function toggleDropdown(event) {
+    button = event.target;
+    string = button.getAttribute("id");
+    id = string.split('_')[0];
+    dropdownContent = document.getElementById(id);
     dropdownContent.classList.toggle('show');
-  }
+}
 
 
 window.onclick = function(event) {
