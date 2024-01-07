669b65473552e29a2298497c1b06e7c04d91f109
AnnaFusari <75632617+AnnaFusari@users.noreply.github.com>
2024-01-07 22:39:46 +0100

    prova

--- code/javascript/MusicKeyBoard.js
+++ code/javascript/MusicKeyBoard.js
@@ -2,23 +2,38 @@ let whiteWidth = 80;
 let blackWidth = whiteWidth/2;
 n=7;
 nscale=2;
+let firstScale = 3; 
 
 
 //servono per definire id di ogni tasto
 const whiteKey = ["C", "D", "E", "F", "G", "A", "B"];
 const blackKey = ["Db", "Eb", "", "Gb", "Ab", "Bb", ];
 
+pianoKeys=whiteKey.concat(blackKey);
+var pianoSamples = createSamplesList(pianoKeys, "pianoSamples", "piano", firstScale);
+
 // questa funzione disegna la key board con nscale scale. (ora 3)
 function drawKeyBoard(container_id) {
     let key = "<svg width=\"4000\" height=\"500\">\n";
-    let firstScale = 3; 
+    
     for (let j = 0; j < nscale; j++){
-        key += drawKey(key, (n * whiteWidth*j), firstScale);
+        key += drawKey(key, (n * whiteWidth*j), firstScale, container_id);
         firstScale = firstScale+1;
     }
     key+="</svg>";
 
-    enableSoundPiano(container_id, 3);
+    let instrument=document.getElementById(container_id);
+    instrument.addEventListener("mousedown", (e) => {
+        const sample = e.target.dataset.note;
+        let instrument = controller.find_instrument_from_view(e);
+        noteon(sample, pianoSamples, instrument);
+    });
+    instrument.addEventListener("mouseup", (e) => {
+        const sample = e.target.dataset.note;
+        let instrument = controller.find_instrument_from_view(e);
+        noteoff(sample, pianoSamples, instrument);
+    });
+
     return key;
 }
 
@@ -26,12 +41,15 @@ function drawKeyBoard(container_id) {
 //passo j per definire l'id di ogni tasto
 
 // questa funzione disegna solo una scala
-function drawKey(key, disp=0, firstScale){
+function drawKey(key, disp=0, firstScale, container_id){
     //ciclo per le white key
     for (let i = 0; i <= n; i++) {
         let x = whiteWidth * i;
         key +=
-            "<rect onmousedown=\"this.style.fill='grey'\" onmouseleave=\"this.style.fill='white'\" onmouseup=\"this.style.fill='white'\" data-note='"+ whiteKey[i] + (firstScale) +"' id="+whiteKey[i] + (firstScale)+" x="+ (x+disp) +" y=\"20\" rx=\"0\" ry=\"0\" width="+whiteWidth+" height=\"350\"\n" +
+            //"<rect onmousedown=\"noteon('"+ whiteKey[i] + (firstScale) +"',  pianoSamples , '"+ container_id +"')\" onmouseleave=\"this.style.fill='white'\" onmouseup=\"noteoff('"+ whiteKey[i] + (firstScale) +"',  pianoSamples , '"+ container_id +"')\" data-note='"+ whiteKey[i] + (firstScale) +"' id="+whiteKey[i] + (firstScale)+" x="+ (x+disp) +" y=\"20\" rx=\"0\" ry=\"0\" width="+whiteWidth+" height=\"350\"\n" +
+            //"  class=\"whiteKey\" />\n";
+
+            "<rect onmousedown=\"this.style.fill='gray'\" onmouseleave=\"this.style.fill='white'\"  onmouseup=\"this.style.fill='white'\" data-note='"+ whiteKey[i] + (firstScale) +"' id="+whiteKey[i] + (firstScale)+" x="+ (x+disp) +" y=\"20\" rx=\"0\" ry=\"0\" width="+whiteWidth+" height=\"350\"\n" +
             "  class=\"whiteKey\" />\n";
     }
 
@@ -39,16 +57,15 @@ function drawKey(key, disp=0, firstScale){
     for (let i = 1; i < n; i++) {
         if (i != 3) {
             let x = (i * whiteWidth) - blackWidth / 2;
-            key += "<rect onmousedown=\"this.style.fill='grey'\" onmouseleave=\"this.style.fill='black'\" onmouseup=\"this.style.fill='black'\" data-note='"+ blackKey[i-1] + (firstScale) +"' id="+blackKey[i-1] + (firstScale)+"  x=" + (x+disp) + " y=\"20\" rx=\"5\" ry=\"5\" width=" + blackWidth + " height=\"200\"\n" +
-                "  class=\"blackKey\" />\n";
+            key += "<rect onmousedown=\"this.style.fill='gray'\" onmouseleave=\"this.style.fill='black'\" onmouseup=\"this.style.fill='black'\" data-note='"+ blackKey[i-1] + (firstScale) +"' id="+blackKey[i-1] + (firstScale)+"  x=" + (x+disp) + " y=\"20\" rx=\"5\" ry=\"5\" width=" + blackWidth + " height=\"200\"\n" +
+            "  class=\"blackKey\" />\n";
+            
+            
+            //"<rect onmousedown=\"noteon('"+ blackKey[i-1] + (firstScale) +"',  pianoSamples , '"+ container_id +"')\" onmouseleave=\"this.style.fill='black'\" onmouseup=\"noteoff('"+ blackKey[i-1] + (firstScale) +"',  pianoSamples , '"+ container_id +"')\" data-note='"+ blackKey[i-1] + (firstScale) +"' id="+blackKey[i-1] + (firstScale)+"  x=" + (x+disp) + " y=\"20\" rx=\"5\" ry=\"5\" width=" + blackWidth + " height=\"200\"\n" +
+                //"  class=\"blackKey\" />\n";
         }
     }
     return key;
 }
 
-function enableSoundPiano(container_id, firstScale){
-    pianoKeys=whiteKey.concat(blackKey);
-    let pianoSamples = createSamplesList(pianoKeys, "pianoSamples", "piano", firstScale);
-    console.log(pianoSamples);
-    loadSound(pianoSamples, container_id );
-}
+
